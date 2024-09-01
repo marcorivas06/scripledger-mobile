27cec7d27162174f82cb5b70bffbd11e9c28b215
marcorivas06 <MRIVASR@emory.edu>
2024-08-31 21:53:59 -0400

    addead EAS and Transactions

--- src/pages/Tabs/screens/Start/Start.tsx
+++ src/pages/Tabs/screens/Start/Start.tsx
@@ -3,23 +3,29 @@ import jsonForAccountData from "@src/mock/accountResponse.json";
 import jsonForTransactions from "@src/mock/accountResponseTransactions.json";
 //------
 import { CircularButton } from "@components/atoms/CircularButton";
-import { Page, MyHeader, Section, AnimatedSection } from "@components/molecules/Page";
+import {
+  Page,
+  MyHeader,
+  Section,
+  AnimatedSection,
+} from "@components/molecules/Page";
 import { Button, HStack, Text, View, VStack } from "@gluestack-ui/themed";
 import { useEffect, useRef, useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { IBalance, IStartActionButton, ITransaction } from "@types/types";
 import { StartActionButtons } from "@components/molecules/StartActionButtons";
-import { HorizontalGiftCardTile, VerticalGiftCardTile } from "@components/molecules/GiftCardTile";
+import {
+  HorizontalGiftCardTile,
+  VerticalGiftCardTile,
+} from "@components/molecules/GiftCardTile";
 import { TransactionHistory } from "@components/molecules/TransactionHistory";
 import { SCREENS } from "@constants";
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import { TouchableOpacity, StyleSheet } from "react-native";
 import { TabIcon } from "@components/atoms/TabIcon";
 import { STACKS } from "@types/routes";
+import { test } from '@src/helper/utils'
 
-
-import * as Crypto from 'expo-crypto';
-import { ethers } from "ethers";
-// 
+//
 
 // Would come from a request
 export function Start({ navigation }) {
@@ -27,48 +33,6 @@ export function Start({ navigation }) {
   const [balances, setBalances] = useState<IBalance[]>([]);
   const [transactions, setTransactions] = useState<ITransactions[]>([]);
 
-  // ---
-  //Generate Mnemonic generates the phrase. 
-  async function generateMnemonic(){
-    const randomBytes = await Crypto.getRandomBytesAsync(32);
-    const mnemonic = ethers.Mnemonic.fromEntropy(randomBytes)
-    return mnemonic.phrase;
-  };
-
-
-async function mnemonicToSeed(mnemonic: string){
-    const bip39 = await import("bip39");
-    const seed = await bip39.mnemonicToSeed(mnemonic);
-    return Buffer.from(seed).toString("hex");
-  };
-
-  async function test() {
-    try {
-      
-      const mnemonic = await generateMnemonic();
-      const seed = await mnemonicToSeed(mnemonic);
-      console.log(seed);
-
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
-  test();
-
-  // mnemonicToSeed(x);
-
-  
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const x  = await generateMnemonic();
-  //     console.log(x)
-  //   })();
-    
-  // }, []);
-  
-  
   // ----
   useEffect(() => {
     const { balances } = jsonForAccountData;
@@ -78,25 +42,33 @@ async function mnemonicToSeed(mnemonic: string){
     setTransactions(transactions);
   }, []);
 
+  useEffect(() => {
+    try {
+      test()
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
+
   const navigateToScreenHidingTabs = (screen) => {
     navigation.navigate(screen, {
-      hideTabs:true
+      hideTabs: true,
     });
   };
-  
-  const navigateToScreen = (screen ) => {
+
+  const navigateToScreen = (screen) => {
     navigation.navigate(screen);
   };
 
   const handleSeeAllBalances = (screen) => {
-    navigation.navigate(screen, { 
-      balances: balances
+    navigation.navigate(screen, {
+      balances: balances,
     });
   };
-  
+
   const handleSettings = () => {
     navigation.navigate(STACKS.MODAL, {
-      screen: SCREENS.MODAL_STACK.SETTINGS
+      screen: SCREENS.MODAL_STACK.SETTINGS,
     });
   };
 
@@ -106,28 +78,30 @@ async function mnemonicToSeed(mnemonic: string){
       iconName: "line-scan", // Replace with the actual icon name or SVG path
       iconAs: "MaterialCommunityIcons", // Replace with the actual icon name or SVG path
       gradientColor: ["#2A864F", "#1BCE63"],
-      action: () => { navigateToScreenHidingTabs(SCREENS.START_STACK.ADD) }
+      action: () => {
+        navigateToScreenHidingTabs(SCREENS.START_STACK.ADD);
+      },
     },
     {
       name: "Pay",
       iconName: "wallet", // Replace with the actual icon name or SVG path
       iconAs: "AntDesign", // Replace with the actual icon name or SVG path
       gradientColor: ["#F45B89", "#FD8FB0"],
-      action: () =>  navigateToScreen(SCREENS.START_STACK.PAY)
+      action: () => navigateToScreen(SCREENS.START_STACK.PAY),
     },
     {
       name: "Send",
       iconName: "email-send", // Replace with the actual icon name or SVG path
       iconAs: "MaterialCommunityIcons", // Replace with the actual icon name or SVG path
       gradientColor: ["#B14FFF", "#CA8FF8"],
-      action: () =>  handleSeeAllBalances(SCREENS.START_STACK.SEND)
+      action: () => handleSeeAllBalances(SCREENS.START_STACK.SEND),
     },
     {
       name: "Receive",
       iconName: "hand-holding-dollar", // Replace with the actual icon name or SVG path
       iconAs: "FontAwesome6", // Replace with the actual icon name or SVG path
       gradientColor: ["#6DC0FC", "#258CD6"],
-      action: () =>  navigateToScreen(SCREENS.START_STACK.RECEIVE)
+      action: () => navigateToScreen(SCREENS.START_STACK.RECEIVE),
     },
   ];
 
@@ -139,7 +113,12 @@ async function mnemonicToSeed(mnemonic: string){
           userName="Marcos"
           isHomePage={true}
           rightHeaderComponent={
-            <CircularButton name="settings" as="Feather" radius="$full" onPress={handleSettings} />
+            <CircularButton
+              name="settings"
+              as="Feather"
+              radius="$full"
+              onPress={handleSettings}
+            />
           }
         />
         <Section isHigherOpacity={true}>
@@ -147,10 +126,17 @@ async function mnemonicToSeed(mnemonic: string){
             title="My Wallet"
             marginBottom={15}
             rightHeaderComponent={
-              <TouchableOpacity onPress={() => handleSeeAllBalances(SCREENS.START_STACK.ALL_GIFTCARDS)} style={styles.goBackButton}>
+              <TouchableOpacity
+                onPress={() =>
+                  handleSeeAllBalances(SCREENS.START_STACK.ALL_GIFTCARDS)
+                }
+                style={styles.goBackButton}
+              >
                 <HStack alignItems="center">
                   <TabIcon as="MaterialCommunityIcons" name="eye" size="xs" />
-                  <Text marginHorizontal={5} style={styles.goBackText}>See All</Text>
+                  <Text marginHorizontal={5} style={styles.goBackText}>
+                    See All
+                  </Text>
                 </HStack>
               </TouchableOpacity>
             }
@@ -169,17 +155,16 @@ async function mnemonicToSeed(mnemonic: string){
 
 const styles = StyleSheet.create({
   goBackButton: {
-    backgroundColor: '#FFF1FF', // Change this to your desired background color
+    backgroundColor: "#FFF1FF", // Change this to your desired background color
     padding: 10,
     borderRadius: 15,
-    alignItems: 'center',
+    alignItems: "center",
     margin: 10,
-    width:'30%',
-    
+    width: "30%",
   },
   goBackText: {
-    fontFamily: 'DarkerGrotesque-Medium',
+    fontFamily: "DarkerGrotesque-Medium",
     fontSize: 15,
-    color:'#51382F'
+    color: "#51382F",
   },
-  });
+});
